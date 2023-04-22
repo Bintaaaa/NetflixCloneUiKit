c3a95fae05ae9ef7eb3d0bcdbdaf9bb0188fd03c
Bintaaaa <bijantium.yt.32@gmail.com>
2023-04-22 13:55:07 +0700

    first

---
+++ NetflixCloneSwiftUI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NetflixCloneSwiftUI
+//
+//  Created by Bijantyum on 22/04/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
